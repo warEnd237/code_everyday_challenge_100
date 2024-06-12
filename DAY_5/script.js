f4ba9654ecc8f8a2c0d5912973952540f6fd7ba3
Kamgaing Bobda Joseph Warren <lebobi35@gmail.com>
2024-06-12 12:49:17 +0100

    ce qu'il fallait faire

--- DAY_5/script.js
+++ DAY_5/script.js
@@ -1,22 +1,22 @@
-const mask = document.querySelector('.mask');
-let i = 0;
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
 
-document.addEventListener('DOMContentLoaded', () => {
-    
-    mask.classList.add('active');
+let load = 0;
 
-    setInterval(() => {
+let int = setInterval(blurring, 30);
 
-        if(i <= 100){
+function blurring() {
+    load++
 
-            mask.innerText = i + "%";
-                
-            console.log(i);
+    if(load > 99) {
+        clearInterval(int);
+    }
 
-            i++
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity= scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
 
-            }
-            
-        }, 90)
-    
-})
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+};
